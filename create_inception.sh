c06ec3a832a8515cb87864ee19deccda3e153974
Luna-wl <sky@Warintorns-MacBook-Air.local>
2023-11-20 21:10:12 +0700

    update: dir

--- create_inception.sh
+++ create_inception.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
+mkdir ft_inception
+touch ft_inception/Makefile
+mkdir ft_inception/srcs
+touch ft_inception/srcs/docker-compose.yml \
+		ft_inception/srcs/.env.sample
 
-touch Makefile
-mkdir srcs
-touch srcs/docker-compose.yml \
-		srcs/.env.sample
-
-mkdir -p srcs/requirements/bonus \
-		srcs/requirements/nginx \
-		srcs/requirements/nginx/tools \
-		srcs/requirements/nginx/conf \
-		srcs/requirements/mariadb \
-		srcs/requirements/mariadb/tools \
-		srcs/requirements/mariadb/conf \
-		srcs/requirements/wordpress \
-		srcs/requirements/wordpress/tools \
-		srcs/requirements/wordpress/conf
+mkdir -p ft_inception/srcs/requirements/bonus \
+		ft_inception/srcs/requirements/nginx \
+		ft_inception/srcs/requirements/nginx/tools \
+		ft_inception/srcs/requirements/nginx/conf \
+		ft_inception/srcs/requirements/mariadb \
+		ft_inception/srcs/requirements/mariadb/tools \
+		ft_inception/srcs/requirements/mariadb/conf \
+		ft_inception/srcs/requirements/wordpress \
+		ft_inception/srcs/requirements/wordpress/tools \
+		ft_inception/srcs/requirements/wordpress/conf
 		
-touch srcs/requirements/nginx/Dockerfile \
-		srcs/requirements/nginx/.dockerignore \
-		srcs/requirements/mariadb/Dockerfile \
-		srcs/requirements/mariadb/.dockerignore \
-		srcs/requirements/wordpress/Dockerfile \
-		srcs/requirements/wordpress/.dockerignore
+touch ft_inception/srcs/requirements/nginx/Dockerfile \
+		ft_inception/srcs/requirements/nginx/.dockerignore \
+		ft_inception/srcs/requirements/mariadb/Dockerfile \
+		ft_inception/srcs/requirements/mariadb/.dockerignore \
+		ft_inception/srcs/requirements/wordpress/Dockerfile \
+		ft_inception/srcs/requirements/wordpress/.dockerignore
 
 
